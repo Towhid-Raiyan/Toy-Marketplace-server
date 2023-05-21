fed48e0bbdcb995da4d9a101db68c7519c98b546
Towhid-Raiyan <towhid.raiyan.com>
2023-05-21 21:51:32 +0600

    get  a toy for tab function added

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ async function run() {
       res.send(result);
     });
 
+    // get a toy
+    app.get("/toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+  });
+
 
 
     // Send a ping to confirm a successful connection
